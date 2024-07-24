691ba68b604fa2e850d4c5e7af7f1d6a321844c1
zerias52 <bbyrd52@gmail.com>
2024-07-24 09:37:36 -0600

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+var num1 = prompt("Enter your first number:");
+var num2 = prompt("Enter your second number:");
+var operation = prompt(" Menu : 1. Sum - 2. Subs - 3. Div - 4. Mult");
+if(operation=="1"){
+    var answer=Number(num1)+Number(num2);//sum operation
+    var operator = "+"
+}
+else if(operation=="2"){
+    var answer=Number(num1)-Number(num2);
+    var operator = "-"
+
+}
+else if(operation=="3"){
+    var answer=Number(num1)/Number(num2);
+    var operator = "/"
+    if (num1==0||num2==0) {
+        var answer = "0";
+    }
+}
+else {
+    var answer=Number(num1)*Number(num2);
+    var operator = "*"
+}
+
+
+document.write(num1 + " " + operator + " " + num2 + " = " + answer  ); //display the result
